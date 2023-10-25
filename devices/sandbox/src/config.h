9871ce67ef1d9918cab3d7d3ba170c059546e01d
Sam <samakj@live.co.uk>
2023-10-25 12:11:45 +0100

    temp

---
+++ devices/sandbox/src/config.h
@@ -0,0 +1,14 @@
+#define HOSTNAME "sandbox"
+#define IP_ADDRESS "192.168.1.111"
+
+#define PURLEY_PARK_SSID "Patty"
+#define PURLEY_PARK_PASSWORD "secrets.users.wifi.patty.password"
+
+#define THE_VALE_SSID "VM1410613"
+#define THE_VALE_PASSWORD "secrets.users.wifi.VM1410613.password"
+
+#define NTP_SERVER "uk.pool.ntp.org"
+
+#define OTA_PASSWORD "WAW>A8GGppz;7&7m"
+
+#define DHT_PIN 13
